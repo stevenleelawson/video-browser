6dfd496d268b4a585aead96ba1b19464a50583d3
Steven Lawson <steven.lawson@healthsqyre.com>
2024-05-01 15:10:57 -0600

    adding axios and apikey

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div id="app">
-   <SearchBar />
+   <SearchBar v-on:termChange="onTermChange"/>
   </div>
 </template>
 
 <script>
 import SearchBar from './components/SearchBar.vue';
+const API_KEY = 'AIzaSyDjn1izn3Td1YVuyuCwX13JwNn8e6cPvoE';
+
 export default {
   // providing name prop helps with debuggging
   name: 'App',
   components: {
     SearchBar
+  },
+  data() {
+    return {
+      term: ''
+    }
+  },
+  methods: {
+    // SAME AS onTermChange: function() {}
+    onTermChange(searchTerm) {
+      this.term = searchTerm;
+    }
   }
 };
 </script>

10d347a20c544492f47e0d19e4c271d93500118d
Steven Lawson <steven.lawson@healthsqyre.com>
2024-05-07 12:42:48 -0600

    getting res from youtube api and rendering what user types in search bar

---
+++ src/components/VideoList.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<ul class="video-list">
+			<VideoListItem
+				v-for="video in videos"
+				:video="video"
+				:key="video.etag"
+			></VideoListItem>
+		</ul>
+	</div>
+</template>
+
+<script>
+import VideoListItem from './VideoListItem.vue';
+export default {
+	name: 'VideoList',
+	props: [ 'videos'],
+	components: {
+		VideoListItem,
+	}
+}
+</script>
+
+<style>
+
+</style>
